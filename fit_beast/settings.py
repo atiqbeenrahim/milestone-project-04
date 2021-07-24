e40061c413a3d3c68b47ccd5416671a032e423d5
atiqbeenrahim <abr.royal@gmail.com>
2021-07-24 00:33:31 +0000

    Remove secret key and set debug

--- fit_beast/settings.py
+++ fit_beast/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-64w-uz7=7*^ov$6uo$s089a1kdy4^ph1b6q0k+=)+z(8^7*&@c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone-project-04.herokuapp.com', 'localhost']
 
