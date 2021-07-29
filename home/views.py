03a0ec00c65dd96dc8cb24cb4c0c886377ffefa5
atiqbeenrahim <abr.royal@gmail.com>
2021-07-29 10:52:31 +0000

    added 404 and 500 html page in templates

--- home/views.py
+++ home/views.py
@@ -4,3 +4,6 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def error_404_view(request, exception):
+    return render(request,'404.html')
